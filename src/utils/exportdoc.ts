fea4a695acc0ea30cec9d9722659f9a4acd9df45
Big-Pabby <victoradekunle312@gmail.com>
2025-04-12 11:26:41 +0100

    fix deployment issue

--- src/utils/exportdoc.ts
+++ src/utils/exportdoc.ts
@@ -1,15 +1,15 @@
-import { Document, Packer, Paragraph } from 'docx';
-import { saveAs } from 'file-saver';
+// import { Document, Packer, Paragraph } from 'docx';
+// import { saveAs } from 'file-saver';
 
-export const exportHtmlToDocx = async (html: string) => {
-  const doc = new Document({
-    sections: [
-      {
-        children: [new Paragraph({ text: html.replace(/<[^>]*>?/gm, '') })],
-      },
-    ],
-  });
+// export const exportHtmlToDocx = async (html: string) => {
+//   const doc = new Document({
+//     sections: [
+//       {
+//         children: [new Paragraph({ text: html.replace(/<[^>]*>?/gm, '') })],
+//       },
+//     ],
+//   });
 
-  const blob = await Packer.toBlob(doc);
-  saveAs(blob, 'document.docx');
-};
+//   const blob = await Packer.toBlob(doc);
+//   saveAs(blob, 'document.docx');
+// };
